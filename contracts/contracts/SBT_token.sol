be6f6b04d62e9a2f0bdbfb80e3ff25b838fd28e1
sarthak18t <sarthaktailor@gmail.com>
2023-12-09 00:41:59 +0530

    SBT_token contract added

---
+++ contracts/contracts/SBT_token.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract newone is ERC721, Ownable {
+   uint256 private _tokenIdCounter;
+
+   enum Role { Contributor, Trader, Investor, Company }
+
+   mapping(address => string) private _githubUsernames;
+   mapping(string => bool) private _githubUsernameTaken;
+   mapping(address => bool) private _walletHasToken;
+   mapping(uint256 => Role) private _tokenRoles;
+
+   constructor(address initialOwner) ERC721("SoulBoundToken", "SBT") Ownable(initialOwner) {}
+
+   function safeMint(address to, Role role, string memory githubUsername) public onlyOwner {
+       require(bytes(githubUsername).length > 0, "GitHub username cannot be empty");
+       require(!_walletHasToken[to], "Wallet address already has a token");
+       require(!_githubUsernameTaken[githubUsername], "GitHub username already taken");
+       uint256 tokenId = _tokenIdCounter;
+       _safeMint(to, tokenId);
+       _tokenIdCounter += 1;
+
+       _tokenRoles[tokenId] = role;
+       _githubUsernames[to] = githubUsername;
+       _walletHasToken[to] = true;
+       _githubUsernameTaken[githubUsername] = true;
+   }
+
+   function getRole(uint256 tokenId) public view returns (Role) {
+       return _tokenRoles[tokenId];
+   }
+
+   function getGitHubUsername(address wallet) public view returns (string memory) {
+       return _githubUsernames[wallet];
+   }
+
+   function burn(uint256 tokenId) external onlyOwner {
+       address owner = ownerOf(tokenId);
+       require(owner == msg.sender, "Only the owner of the token can burn it.");
+       _burn(tokenId);
+
+       // Clean up mappings after burning
+       delete _tokenRoles[tokenId];
+       delete _githubUsernames[owner];
+       _walletHasToken[owner] = false;
+   }
+
+   function _transfer(address from, address to, uint256 tokenId) internal virtual override {
+       require(from == address(0) || to == address(0), "This is a Soulbound token. It cannot be transferred. It can only be burned by the token owner.");
+       super._transfer(from, to, tokenId);
+   }
+}
+
