74e125e617f7ffae6f54f590b87854d399063e60
dharmikjethva30 <dharmikjethva30>
2023-12-08 23:22:43 +0530

    added github apis

---
+++ server/app.js
@@ -0,0 +1,141 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const axios = require('axios')
+const mongoose = require('mongoose')
+
+
+const functions = require('./functions')
+
+const PORT = 3000
+const app = express()
+
+
+
+// middlewares
+app.use(bodyParser.json());
+app.use(cors())
+
+const connect = () => {
+  mongoose.set('strictQuery', true)
+  mongoose.connect('mongodb+srv://dharmikjethva4:ethindia@cluster0.39au37z.mongodb.net/?retryWrites=true&w=majority')
+      .then(() => {
+          console.log("Connected to database");
+      })
+      .catch((err) => {
+          console.log("[!ERROR]")
+          console.log(err);
+          throw new Error
+      })
+}
+
+app.get('/', (req, res) => {
+    console.log(req.headers);
+    res.status(200).send("hello")
+})
+
+app.post('/createCompany', (req, res) => {
+    console.log(req.body);
+    const {name, token, walletAddress} = req.body;
+    const response = functions.createCompany(name, token, walletAddress)
+
+    res.status(200).send(response)
+
+})
+
+
+app.post('/createRepo', (req, res) => {
+    console.log(req.body);
+    const {name, description, token} = req.body;
+    const response = functions.createRepo(name,description, token)
+
+    res.status(200).send(response)
+
+})
+
+app.post('/createIssue', async(req, res) => {
+    console.log(req.body);
+    const {owner, repo, title, body,token} = req.body;
+    const response = await functions.createIssue(owner, repo, title, body,token)
+    console.log(response);
+
+    res.status(200).send(response)
+
+})
+
+
+
+app.post('/getOpenIssue', async(req, res) => {
+  const {owner, repo,token} = req.body;
+  const response = await functions.getIssues(owner, repo,token)
+  
+
+  res.status(200).send(response)
+
+})
+
+
+
+
+app.post('/addCollaborator', async(req, res) => {
+    const {owner, repo,assignee, token} = req.body;
+    const response = await functions.addCollaborator(owner, repo, assignee, token)
+    res.status(200).send(response)
+
+})
+
+app.post('/addAssignee', async(req, res) => {
+    const {owner, repo, issue_number,assignee, token} = req.body;
+    const response = await functions.assignIssues(owner, repo, issue_number,assignee, token)
+    res.status(200).send(response)
+
+})
+
+app.post('/api/check_pr_merged', async (req, res) => {
+    const { token, issue_number, owner, repo } = req.body;
+  
+    if (!token || !issue_number || !owner || !repo) {
+      return res.status(400).send({
+        message: 'Missing required parameters',
+      });
+    }
+  
+    try {
+      const response = await axios.get(
+        `https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    //   console.log(JSON.parse(response.pull_request));
+      const pullRequestUrl = response.pull_request.url;
+  
+      const pullRequestResponse = await axios.get(pullRequestUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      const merged = pullRequestResponse.data.merged;
+  
+      res.status(200).send({
+        merged,
+        message: merged
+          ? 'Pull request is merged'
+          : 'Pull request is not merged',
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: 'Error checking pull request status',
+      });
+    }
+  });
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`)
+    connect()
+})
+
---
+++ server/functions.js
@@ -0,0 +1,117 @@
+const { Octokit } = require("octokit");
+const Company = require('./schema');
+const { Mongoose } = require("mongoose");
+
+const getIssues = async (owner, repo, token) => {
+  const octokit = new Octokit({
+    auth: token,
+  });
+  const response = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+    owner: owner,
+    repo: repo,
+  });
+
+  const filteredIssues = response.filter(issue => issue.state === 'open');
+  return filteredIssues
+};
+
+const createCompany = async(username, token, walletAddress)=>{
+  const company = new Company({
+    name:username,
+    token:token,
+    walletAddress:walletAddress
+  })
+  await company.save()
+}
+
+
+
+const addCollaborator = async (owner, repo ,assignee, token) => {
+    const octokit = new Octokit({
+      auth: token,
+    });
+  
+    await octokit.request('PUT /repos/{owner}/{repo}/collaborators/{username}', {
+      owner: owner,
+      repo: repo,
+      username: assignee,
+      permission: 'pull',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+}
+
+const assignIssues = async (owner, repo, issue_number,assignee) => {
+  const userCompany = await Company.findOne({username:owner})
+  const token = userCompany.token;
+
+  const octokit = new Octokit({
+    auth: token,
+  });
+
+  await octokit.request('PUT /repos/{owner}/{repo}/collaborators/{username}', {
+    owner: owner,
+    repo: repo,
+    username: assignee,
+    permission: 'pull',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+
+  return await octokit.request(
+    "POST /repos/{owner}/{repo}/issues/{issue_number}/assignees",
+    {
+      owner: owner,
+      repo: repo,
+      issue_number: issue_number,
+      assignees: [assignee],
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+};
+
+const createIssue = async (owner, repo, title, body, token) => {
+  try {
+    const octokit = new Octokit({
+      auth: token,
+    });
+
+    return await octokit.request("POST /repos/{owner}/{repo}/issues", {
+      owner: owner,
+      repo: repo,
+      title: title,
+      body: body,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+  } catch (error) {
+    return error;
+  }
+};
+
+const createRepo = async (name, description, token) => {
+  try {
+    const octokit = new Octokit({
+      auth: token,
+    });
+
+    await octokit.request("POST /user/repos", {
+      name: `chamber-${name}`,
+      description: description,
+      homepage: "https://github.com",
+      private: true,
+      is_template: true,
+    });
+    return true
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getIssues, createIssue, createRepo, assignIssues, addCollaborator, getIssues, createCompany };
