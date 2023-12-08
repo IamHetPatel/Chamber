27a163d726dd49cf09de9f1d633020ea1714301f
sarthak18t <sarthaktailor@gmail.com>
2023-12-09 01:09:33 +0530

    contract Data created

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -13,7 +13,7 @@ async function main() {
 
   function saveFrontendFiles(contract, name) {
     const fs = require("fs");
-    const contractsDir = __dirname + "/contractsData";
+    const contractsDir = __dirname + "/../../client/contractData";
   
     if (!fs.existsSync(contractsDir)) {
       fs.mkdirSync(contractsDir);
