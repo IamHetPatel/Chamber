12966d6643be9b5b21660015f4f54c908355ddbc
sarthak18t <sarthaktailor@gmail.com>
2023-12-09 05:43:27 +0530

    dao contract deployed

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -6,10 +6,16 @@ async function main() {
 
   console.log("Deploying contracts with account : ",await deployer.address);
   const sbt = await hre.ethers.getContractFactory("newone");
+  const dao = await hre.ethers.getContractFactory("DAO");
+
   const sbt_deploy = await sbt.deploy(deployer);
+  const dao_deploy = await dao.deploy(deployer);
 
   console.log("SBT contract is deployed on address ",await sbt_deploy.getAddress());
+  console.log("DAO contract is deployed on address ",await dao_deploy.getAddress());
+
   saveFrontendFiles(sbt_deploy , "newone");
+  saveFrontendFiles(dao_deploy , "DAO");
 
   function saveFrontendFiles(contract, name) {
     const fs = require("fs");
