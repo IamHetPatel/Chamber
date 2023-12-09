a9a1bca58a53286e4efb9b6bb04abff494bfd00d
sarthak18t <sarthaktailor@gmail.com>
2023-12-09 18:05:43 +0530

    developerPage modified

--- client/src/pages/DeveloperPage.jsx
+++ client/src/pages/DeveloperPage.jsx
@@ -1,13 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/developerPage.css";
-// import Issue from '../components/Issue'
 import ProjectModal from "../components/developer/ProjectModal";
 import ExploreItem from "../components/developer/ExploreItem";
+import { readContract, writeContract } from "@wagmi/core";
+import { contract_address as sbt_address } from "../../contractData/newone-address.json";
+import { abi as sbt_abi } from "../../contractData/newone.json";
+import { abi as dao_abi } from "../../contractData/DAO.json";
+import { contract_address as dao_address } from "../../contractData/DAO-address.json";
 
 const DeveloperPage = () => {
+  const [selectedProjectId, setSelectedProjectId] = useState(null);
+  const [projects, setProjects] = useState([]);
   const [openModal, setOpenModal] = useState(false);
   const [selectedTitle, setSelectedTitle] = useState("");
   const [selectedId, setSelectedId] = useState("");
+
+// Fetch projects from DAO API when the component mounts
+const fetchProjects = async () => {
+  try {
+    console.log(123)
+    const response = await readContract({
+      abi: dao_abi, // The ABI for your DAO contract
+      address: dao_address, // The address of your DAO contract
+      functionName: "getDAO", // The function name to call
+    });
+    const data = response;
+    // console.log(data)
+    // console.log(123)
+
+    if (Array.isArray(data)) {
+      const projectDetails = data.map((project) => ({
+        id: project.id.toString(), // Convert id to string if it's a BigInt
+        name: project.name,
+        pool: 1000,
+        tokens: 60 // Use an empty string if uri is undefined
+      }));
+      setProjects(projectDetails);
+    } else {
+      console.error("Data is not an array:", data);
+    }
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+  }
+};
+
+const createIssue = 
+
+useEffect(() => {
+  fetchProjects();
+}, []);
+
   return (
     <>
       <div className="developer-page-container">
@@ -27,15 +69,21 @@ const DeveloperPage = () => {
               <div className="explore-column-title">Tokens</div>
             </div>
             <div className="explore-list">
-              <ExploreItem
-                id={101}
-                name={"Project 1"}
-                pool={100000}
-                tokens={5}
-                setOpenModal={setOpenModal}
-                setSelectedTitle={setSelectedTitle}
-                setSelectedId={setSelectedId}
-              />
+            {projects?.map(({ id, name, pool, tokens }) => {
+                return(
+                   <ExploreItem
+                    key={id}
+                    id={id}
+                    name={name}
+                    pool={pool}
+                    tokens={tokens}
+                    setOpenModal={setOpenModal}
+                    setSelectedTitle={setSelectedTitle}
+                    setSelectedId={setSelectedId}
+                  />
+                )
+                })} 
+              
             </div>
           </div>
         </div>
